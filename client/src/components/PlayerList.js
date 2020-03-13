42bd55070c4507f08448c5fd534800feee565dd2
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-03-13 10:22:17 -0600

    Test PlayerList component fetch and display

--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -2,15 +2,25 @@ import React from "react";
 import axios from "axios";
 
 class PlayerList extends React.Component {
+  _isMounted = false;
   state = {
     players: []
   };
 
   componentDidMount() {
+    this._isMounted = true;
     axios
       .get("http://localhost:5000/api/players")
-      .then(res => this.setState({ players: res.data }))
-      .catch(console.log("Failed to fetch player list"));
+      .then(res => {
+        if (this._isMounted) {
+          this.setState({ players: res.data });
+        }
+      })
+      .catch(err => console.log("Failed to fetch player list", err));
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
   }
 
   render() {
@@ -19,7 +29,9 @@ class PlayerList extends React.Component {
         <h3>Runner List</h3>
         <div>
           {this.state.players.map(player => (
-            <div><code>{JSON.stringify(player)}</code></div>
+            <div key={player.id}>
+              <code>{JSON.stringify(player)}</code>
+            </div>
           ))}
         </div>
       </div>
---
+++ client/src/components/PlayerList.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { render, cleanup, waitForElement } from "@testing-library/react";
+import PlayerList from "./PlayerList";
+import axiosMock from "axios";
+
+afterEach(cleanup);
+
+it("fetches and displays data", async () => {
+  const playerList = [
+    {
+      name: "Billy Joel",
+      country: "United States",
+      searches: 100000,
+      id: 1337
+    },
+    { name: "Weird Al", country: "United States", searches: 100, id: 9001 }
+  ];
+  axiosMock.get.mockResolvedValueOnce({
+    data: playerList
+  });
+
+  const { getByText } = render(<PlayerList />);
+
+  const playerElement = await waitForElement(() =>
+    getByText(new RegExp(playerList[0].name))
+  );
+
+  expect(playerElement).toBeTruthy();
+  expect(axiosMock.get).toHaveBeenCalledTimes(1);
+});
